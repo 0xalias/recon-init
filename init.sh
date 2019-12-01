3ac96b04f6f65973987325bcc38a962af56da461
0xalias <x@0xalias.com>
2019-12-01 08:08:35 -0800

    cleanup, take GH_TOKEN as arg

--- init.sh
+++ init.sh
@@ -1,17 +1,49 @@
 #!/bin/bash
 
-# One time OS setup for recon.
+# One time OS setup for reconaissance.
 
 set -e
 
-USER=$(cat user)
-USER_HOME="/home/$USER"
+# check for args
+usage() {
+    echo ""
+    echo "Usage: init.sh [GH_TOKEN]"
+    echo ""
+    exit 1
+}
+
+if [[ -z "$1" ]]; then
+    usage
+fi
+
+DT="$(date +%Y%m%d%H%M%S)"
+
+# OS Variables
+OS_USER="0xalias"
+OS_USER_HOME="/home/$OS_USER"
+SSH_KEY_PATH="$OS_USER_HOME/.ssh/id_rsa"
+EMAIL="x@0xalias.com"
 PACKAGES=$(cat packages)
+
+# GH variables
+GH_USER="0xalias"
+GH_KEYS_API_ENDPOINT="https://api.github.com/user/keys"
+GH_KEY_NAME="${GH_USER}-${DT}"
+GH_TOKEN="$1" # do-recon token
 
 printf "\nStarting setup...\n\n"
 
+# create new ssh key
+printf "\nCreating SSH key\n"
+ssh-keygen -t rsa -b 4096 -C "$EMAIL" -f $SSH_KEY_PATH -q -N ''
+
+# push new ssh key to GH
+printf "\nPushing SSH key to GH\n"
+KEY_CONTENTS="$(cat $SSH_KEY_PATH)"
+curl -u "${GH_USER}:${GH_TOKEN}" -d "{\"title\":\"${GH_KEY_NAME}\",\"key\":\"${KEY_CONTENTS}\"}" "$GH_KEYS_API_ENDPOINT"
+
 # OS update
-printf "\nUpdating the OS\n"
+printf "\nUpdating the OS packages\n"
 apt update
 apt upgrade -y
 
@@ -19,17 +51,20 @@ apt upgrade -y
 printf "\nInstalling OS packages:\n"
 for p in "${PACKAGES[@]}"; do echo " --> $p"; done
 echo ""
-apt install -y ${PACKAGES[@]}
+apt install -y "${PACKAGES[@]}"
+
+# add group
+printf "\nAdding %s group\n" $OS_USER
+add group $OS_USER
 
 # add user
-printf "\nAdding $USER user\n"
-useradd -m -s $SH $USER
+printf "\nAdding %s user\n" $OS_USER
+adduser --home $OS_USER_HOME --ingroup $OS_USER --disabled-password $OS_USER
 
 # add user to sudoers
-printf "\nAdding $USER user to sudoers\n"
-usermod -aG sudo $USER
+printf "\nAdding %s user to sudoers\n" $OS_USER
+usermod -aG sudo $OS_USER
 
-# don't prompt for password when using sudo
-echo "$USER ALL=(ALL) NOPASSWD: ALL" | tee /etc/sudoers.d/$USER
-touch $USER_HOME/.sudo_as_admin_successful
-
+# don't prompt for a password when using sudo
+echo "$OS_USER ALL=(ALL) NOPASSWD: ALL" | tee /etc/sudoers.d/$OS_USER
+touch "$OS_USER_HOME/.sudo_as_admin_successful"
